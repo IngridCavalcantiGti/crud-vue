8b19007a1fee4e800c3ab6030037339126c23206
Ingrid <ingridgti08@hotmail.com>
2022-02-07 00:41:15 -0300

    adiciona novo componente do formulario

---
+++ src/components/AddInformation.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="container-fluid"> 
+ <form class="mt-5 ">
+  <div class="row">
+    <div class="col-5">
+      <input type="text" class="form-control" placeholder="Nome">
+    </div>
+    <div class="col-4">
+      <input type="text" class="form-control" placeholder="E-mail">
+    </div>
+     <div class="col-2">
+      <input type="text" class="form-control" placeholder="Telefone">
+    </div>
+    <div class="col-1">
+      <button type="submit" class="btn btn-primary" @click="add">Adicionar</button>
+    </div>
+  </div>
+</form>
+</div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        const add = () => {
+            alert('Add')
+        }
+
+        return { add }
+    },
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
-<div class="d-flex justify-content-center mt-5">
-  <Header/>
+  <div>
+    <div class="d-flex justify-content-center mt-5">
+      <Header/>
+  </div>
+   <AddInformation/>
   </div>
 </template>
 
 <script>
 import  {  defineComponent } from  '@vue/composition-api'
 import Header from '@/components/Header.vue'
+import AddInformation from '@/components/AddInformation.vue'
 
 export  default  defineComponent ( { 
   name: 'Home',
   components: {
-    Header
+    Header,
+    AddInformation
   },
 })
 </script>
